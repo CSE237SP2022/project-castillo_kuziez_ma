d101c414e693b1a92145bd30bbf832f366c52eca
AbdullahK114 <abdullahkuziez@wustl.edu>
2022-04-13 12:07:05 -0500

    committing to allow pulling

--- src/system/System.java
+++ src/system/System.java
@@ -1,10 +1,28 @@
 package system;
 
 import java.util.*;
-
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 public class System {
-	
-	
+	public static void main(String args[]) throws FileNotFoundException {
+		 
+        //creating File instance to reference text file in Java
+        File text = new File("C:/temp/test.txt");
+     
+        //Creating Scanner instance to read File in Java
+        Scanner scnr = new Scanner(text);
+     
+        //Reading each line of the file using Scanner class
+        int lineNumber = 1;
+        while(scnr.hasNextLine()){
+            String line = scnr.nextLine();
+            System.out.println();
+            lineNumber++;
+        }      
+   
+    }  
+
 	
     public LinkedList<Course> courses;
     public LinkedList<Student> students;
