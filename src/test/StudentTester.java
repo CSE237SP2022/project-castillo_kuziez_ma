eefc22a4d65be7b65ce8bf657ba8e22f96c59793
nataliacastillo <nataliacastillo@wustl.edu>
2022-04-12 20:09:56 -0500

    making tests more simple

--- src/test/StudentTester.java
+++ src/test/StudentTester.java
@@ -11,46 +11,35 @@ import system.Student;
 
 class StudentTester {
 
+	LinkedList<Course> BearCourses = new LinkedList<Course>();
+	Student Bear = new Student("12345", "Bear", "WUSTL", 21, BearCourses);
+	
 	@Test
 	void testGetID() {
-		LinkedList<Course> BearCourses = new LinkedList<Course>();
-		Student Bear = new Student("12345", "Bear", "WUSTL", 21, BearCourses);
 		String BearID = Bear.getId();
-		
 		assertTrue(BearID.equals("12345"));
 	}
 
 	@Test
 	void testGetFirstName() {
-		LinkedList<Course> BearCourses = new LinkedList<Course>();
-		Student Bear = new Student("12345", "Bear", "WUSTL", 21, BearCourses);
 		String BearFirstName = Bear.getFirstName();
-		
 		assertTrue(BearFirstName.equals("Bear"));
 	}
 	
 	@Test
 	void testGetLastName() {
-		LinkedList<Course> BearCourses = new LinkedList<Course>();
-		Student Bear = new Student("12345", "Bear", "WUSTL", 21, BearCourses);
 		String BearLastName = Bear.getLastName();
-		
 		assertTrue(BearLastName.equals("WUSTL"));
 	}
 	
 	@Test
 	void testGetAge() {
-		LinkedList<Course> BearCourses = new LinkedList<Course>();
-		Student Bear = new Student("12345", "Bear", "WUSTL", 21, BearCourses);
 		int BearAge = Bear.getAge();
-		
 		assertEquals(BearAge, 21);
 	}
 	
 	@Test
 	void testGetCourseCode() {
-		LinkedList<Course> BearCourses = new LinkedList<Course>();
-		Student Bear = new Student("12345", "Bear", "WUSTL", 21, BearCourses);
 		LinkedList<Student> enrollmentList = new LinkedList<Student>();
 		Course calculus = new Course("Math", "Calculus", 3, enrollmentList);
 		Bear.addCourse(calculus);
